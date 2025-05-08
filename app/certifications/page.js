a84d2998d2f45c141e23d618194997d13fcb2ac6
wncelrcn <rivanowincelarcen@gmail.com>
2025-05-08 21:52:24 +0800

    feat(certifications): added my certifications and its urls

--- app/certifications/page.js
+++ app/certifications/page.js
@@ -89,80 +89,174 @@ const FixedWidthContainer = styled(Box)(({ theme }) => ({
   },
 }));
 
-// Sample certification data (replace with your actual data)
 const mlCertifications = [
   {
     title: "Exploratory Data Analysis for Machine Learning",
     organization: "IBM",
-    certificateUrl: "#",
-  },
-  {
-    title: "Exploratory Data Analysis for Machine Learning",
-    organization: "IBM",
-    certificateUrl: "#",
-  },
-  {
-    title: "Exploratory Data Analysis for Machine Learning",
-    organization: "IBM",
-    certificateUrl: "#",
-  },
-  {
-    title: "Exploratory Data Analysis for Machine Learning",
-    organization: "IBM",
-    certificateUrl: "#",
-  },
-  {
-    title: "Exploratory Data Analysis for Machine Learning",
-    organization: "IBM",
-    certificateUrl: "#",
-  },
-  {
-    title: "Exploratory Data Analysis for Machine Learning",
-    organization: "IBM",
-    certificateUrl: "#",
-  },
-  {
-    title: "Exploratory Data Analysis for Machine Learning",
-    organization: "IBM",
-    certificateUrl: "#",
-  },
-  {
-    title: "Exploratory Data Analysis for Machine Learning",
-    organization: "IBM",
-    certificateUrl: "#",
+    certificateUrl:
+      "https://www.coursera.org/account/accomplishments/verify/XBOVKP7LNE39",
   },
 ];
 
 const cloudCertifications = [
   {
-    title: "Exploratory Data Analysis for Machine Learning",
-    organization: "IBM",
-    certificateUrl: "#",
+    title: "Google Cloud Computing Foundations: Cloud Computing Fundamentals",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9180528?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
   },
   {
-    title: "Exploratory Data Analysis for Machine Learning",
-    organization: "IBM",
-    certificateUrl: "#",
+    title:
+      "Google Cloud Computing Foundations: Networking & Security in Google Cloud",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9384110?qlcampaign=1p-EDUCR-GCCF-MMCL-STUDENT-PH-APR2024-27&utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
   },
   {
-    title: "Exploratory Data Analysis for Machine Learning",
-    organization: "IBM",
-    certificateUrl: "#",
+    title: "Google Cloud Computing Foundations: Infrastructure in Google Cloud",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9378066?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
   },
   {
-    title: "Exploratory Data Analysis for Machine Learning",
-    organization: "IBM",
-    certificateUrl: "#",
+    title:
+      "Google Cloud Computing Foundations: Data, ML, and AI in Google Cloud",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9403909?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+
+  {
+    title: "Attention Mechanism",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9951576?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
   },
   {
-    title: "Exploratory Data Analysis for Machine Learning",
-    organization: "IBM",
-    certificateUrl: "#",
+    title: "Create Image Captioning Models",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9955947?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
   },
   {
-    title: "Exploratory Data Analysis for Machine Learning",
+    title: "Encoder-Decoder Architecture",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9951691?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Introduction to Vertex AI Studio",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9955343?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Transformer Models and BERT Model",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9955998?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Build a Secure Google Cloud Network",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9559758?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Set Up an App Dev Environment on Google Cloud",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9542534?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Responsible AI: Applying AI Principles with Google Cloud",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9566318?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Prepare Data for ML APIs on Google Cloud",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9566280?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Introduction to Responsible AI",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9556909?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Introduction to Large Language Models",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9556824?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Introduction to Image Generation",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9566346?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Introduction to Generative AI",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9556866?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Implement Load Balancing on Compute Engine",
+    organization: "Google Cloud Skills Boost",
+    certificateUrl:
+      "https://www.cloudskillsboost.google/public_profiles/962bc156-2b74-456e-98d5-1dc8461a9cea/badges/9533722?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+];
+
+const webDevCertifications = [
+  {
+    title: "Python for Data Science, AI & Development",
     organization: "IBM",
+    certificateUrl:
+      "https://www.coursera.org/account/accomplishments/verify/AY9JON3ZWI44",
+  },
+];
+
+const pythonCertifications = [
+  {
+    title: "Learn Python Certificate",
+    organization: "Kaggle",
+    certificateUrl:
+      "https://www.kaggle.com/learn/certification/wincelarcen/python",
+  },
+  {
+    title: "Problem Solving (Basic) Certificate",
+    organization: "HackerRank",
+    certificateUrl: "https://www.hackerrank.com/certificates/d9de6349f5c9",
+  },
+  {
+    title: "Python (Basic) Certificate",
+    organization: "HackerRank",
+    certificateUrl: "https://www.hackerrank.com/certificates/9fdbeebcbd17",
+  },
+  {
+    title: "Introduction to Programming Using Python",
+    organization: "CodeChum",
     certificateUrl: "#",
+  },
+];
+
+const otherCertifications = [
+  {
+    title: "Graphic Design Essentials",
+    organization: "Canva",
+    certificateUrl:
+      "https://www.canva.com/design-school/certification-award/22e1b11e-946a-4ed3-80b3-7bf4c47d67f1",
+  },
+  {
+    title: "Getting Started with Technology Startups",
+    organization: "University of Colorado Boulder",
+    certificateUrl:
+      "https://www.coursera.org/account/accomplishments/verify/HN99XSVV99Q4",
   },
 ];
 
@@ -193,10 +287,52 @@ export default function Certifications() {
           </FixedWidthContainer>
         </ScrollableRow>
 
-        <CategoryTitle>CLOUD COMPUTING CERTIFICATIONS</CategoryTitle>
+        <CategoryTitle>WEB DEVELOPMENT CERTIFICATIONS</CategoryTitle>
+        <ScrollableRow>
+          <FixedWidthContainer>
+            {webDevCertifications.map((cert, index) => (
+              <CertificationBox
+                key={index}
+                title={cert.title}
+                organization={cert.organization}
+                certificateUrl={cert.certificateUrl}
+              />
+            ))}
+          </FixedWidthContainer>
+        </ScrollableRow>
+
+        <CategoryTitle>PYTHON CERTIFICATIONS</CategoryTitle>
+        <ScrollableRow>
+          <FixedWidthContainer>
+            {pythonCertifications.map((cert, index) => (
+              <CertificationBox
+                key={index}
+                title={cert.title}
+                organization={cert.organization}
+                certificateUrl={cert.certificateUrl}
+              />
+            ))}
+          </FixedWidthContainer>
+        </ScrollableRow>
+
+        <CategoryTitle>GOOGLE CLOUD CERTIFICATIONS</CategoryTitle>
         <ScrollableRow>
           <FixedWidthContainer>
             {cloudCertifications.map((cert, index) => (
+              <CertificationBox
+                key={index}
+                title={cert.title}
+                organization={cert.organization}
+                certificateUrl={cert.certificateUrl}
+              />
+            ))}
+          </FixedWidthContainer>
+        </ScrollableRow>
+
+        <CategoryTitle>OTHER CERTIFICATIONS</CategoryTitle>
+        <ScrollableRow>
+          <FixedWidthContainer>
+            {otherCertifications.map((cert, index) => (
               <CertificationBox
                 key={index}
                 title={cert.title}
